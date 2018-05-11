7abe029b99a9015049490a3991b55b4bc4e0b478
shurl <shurl@users.noreply.github.com>
2018-05-11 15:56:28 +0100

    Fix FC071 and FC078

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'serverdensity'
 maintainer       'hello@serverdensity.com'
 maintainer_email 'hello@serverdensity.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures the v2 Server Density monitoring agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.1'
